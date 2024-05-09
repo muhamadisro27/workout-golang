2b634f4e97534bbba909aefeefd1bf3610aa5ee2
Muhamad Isro Sabanur <78725683+muhamadisro27@users.noreply.github.com>
2024-05-09 14:17:27 +0000

    add: finish learn comment

---
+++ 1. golang-dasar/comment.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+
+/*
+Ini adalah function main yang mana function 
+yang akan dijalankan pertama kali ketika 
+aplikasi di run !
+**/
+
+func main() {
+
+  // ini komentar
+}
