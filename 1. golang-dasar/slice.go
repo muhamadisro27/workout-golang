3bd11706b7ce6f1b198382ce87b4d664bb2df975
Muhamad Isro Sabanur <78725683+muhamadisro27@users.noreply.github.com>
2024-05-09 10:24:58 +0000

    add: finish learn slice

--- 1. golang-dasar/slice.go
+++ 1. golang-dasar/slice.go
@@ -1,10 +1,25 @@
 package main
 
-// import "fmt"
+import (
+  "fmt"
+)
 
 func main() {
 
-  name := [...]string{"Eko", "Kurniawan", "Khannedy"}
+  // name := [...]string{"Eko", "Kurniawan", "Khannedy", "Joko", "Anwar", "Roozy","Baka","Yaro"}
 
-  fmt.Println(name)
+  // slice := name[:]
+
+  // var slice4  []string = name[4:]
+
+  newSlice := make([]string, 2, 2)
+
+  newSlice[0] = "eko"
+  newSlice[1] = "kurniawan"
+  new := append(newSlice, "Reza")
+  
+  fmt.Println(newSlice, new)
+  fmt.Println(cap(newSlice), len(newSlice))
+  fmt.Println(cap(new), len(new))
+  
 }
--- replit.nix
+++ replit.nix
@@ -1,5 +1,7 @@
 { pkgs }: {
   deps = [
+    pkgs.mysql
+    pkgs.mysql80
     pkgs.go_1_20
     pkgs.cowsay
   ];
