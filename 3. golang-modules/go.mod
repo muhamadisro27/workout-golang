bed941ba493beadc2655153cb5510c0635e22c7c
Muhamad Isro Sabanur <78725683+muhamadisro27@users.noreply.github.com>
2024-05-19 10:25:42 +0000

    feat: adding major changes dependency

--- 3. golang-modules/go.mod
+++ 3. golang-modules/go.mod
@@ -2,4 +2,4 @@ module golang-modules
 
 go 1.20
 
-require github.com/muhamadisro27/say-hello-golang v1.2.0
+require github.com/muhamadisro27/say-hello-golang/v2 v2.0.0 // indirect
--- 3. golang-modules/main.go
+++ 3. golang-modules/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
   "fmt"
-  go_say_hello "github.com/muhamadisro27/say-hello-golang"
+  go_say_hello "github.com/muhamadisro27/say-hello-golang/v2"
 )
 
 func main() {
 
-  fmt.Println(go_say_hello.SayHello())
+  fmt.Println(go_say_hello.SayHello("Roozy"))
   
 }
