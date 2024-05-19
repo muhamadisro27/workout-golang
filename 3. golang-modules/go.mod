6eeae360bf64618816e146b3494775da5f31a28c
Muhamad Isro Sabanur <78725683+muhamadisro27@users.noreply.github.com>
2024-05-19 10:13:51 +0000

    add: finish learn go modules,tag,dependency

---
+++ 3. golang-modules/go.mod
@@ -0,0 +1,5 @@
+module golang-modules
+
+go 1.20
+
+require github.com/muhamadisro27/say-hello-golang v1.2.0
---
+++ 3. golang-modules/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+  "fmt"
+  go_say_hello "github.com/muhamadisro27/say-hello-golang"
+)
+
+func main() {
+
+  fmt.Println(go_say_hello.SayHello())
+  
+}
